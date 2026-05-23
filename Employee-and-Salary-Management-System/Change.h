39bf50febf6b188e6207a12f22abc757d2571ebb
sweet20potato <candy030496@gmail.com>
2026-05-23 10:17:14 +0800

    Fix header guard typo and update includes

--- Employee-and-Salary-Management-System/Change.h
+++ Employee-and-Salary-Management-System/Change.h
@@ -1,12 +1,10 @@
-#ifndef CHANGE_H
+#ifndef CHAMGE_H
 #define CHANGE_H
 
-#include "Read.h"
-
+#include "Employee.h"
 class Change {
 public:
-    void promote(Employee& emp); // 實習升正職
-    void resign(Employee& emp);  // 辭職
+	void promote(Employee& emp);
 };
 
 #endif
