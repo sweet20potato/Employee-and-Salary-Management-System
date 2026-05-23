53d40941ab1e3e5ff77d5762c99a8669bb8a5b8a
sweet20potato <candy030496@gmail.com>
2026-05-23 10:19:54 +0800

    Add Attendance class with leave and late hour management

---
+++ Employee-and-Salary-Management-System/Attendance.h
@@ -0,0 +1,27 @@
+#ifndef ATTENDANCE_H
+#define ATTENDANCE_H
+
+#include <string>
+using namespace std;
+class Attendance
+{
+private:
+	int personalLeave;
+	int sickLeave;
+	int lateHour;
+public:
+	Attendance();
+	Attendance(int, int, int);
+
+	void setPersonalLeave(int);
+	void setSickLeave(int);
+	void setLateHour(int);
+
+	int getPersonalLeave();
+	int getSickLeave();
+	int getLateHour();
+
+	string print();
+};
+
+#endif
