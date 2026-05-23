e0e24b4ac7b6574a16aedb2b9b1d5ab71509ce07
sweet20potato <candy030496@gmail.com>
2026-05-23 10:17:32 +0800

    Refactor promote method in Change.cpp
    Refactored promote method to use getters and setters for Employee attributes.

--- Employee-and-Salary-Management-System/Change.cpp
+++ Employee-and-Salary-Management-System/Change.cpp
@@ -1,19 +1,14 @@
 #include "Change.h"
+#include "Employee.h"
 #include <iostream>
 using namespace std;
 
 void Change::promote(Employee& emp) {
-    if (emp.type == "part") {
-        emp.type = "full";
-        emp.baseSalary = 40000;
-        emp.bonus = 0;
-        cout << emp.name << " 已升為正職！" << endl;
-    } else {
-        cout << emp.name << " 已經是正職。" << endl;
-    }
-}
-
-void Change::resign(Employee& emp) {
-    emp.resigned = true;
-    cout << emp.name << " 已辭職！" << endl;
+	if (emp.getType() == "part") {
+		emp.setType("full");
+		cout << emp.getName() << " got promoted!" << endl;
+	}
+	else {
+		cout << emp.getName() << " is already a full-time employee." << endl;
+	}
 }
