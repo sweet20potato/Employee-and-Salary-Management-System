8aa8e7640915e7c8246ce78066c383d561dae83f
sweet20potato <candy030496@gmail.com>
2026-06-01 12:43:47 +0800

    Implement Employee class with basic functionality

---
+++ Employee-and-Salary-Management-System/Employee.cpp
@@ -0,0 +1,40 @@
+#include "Employee.h"
+#include <string>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int Employee::numOfEmployee = 0;
+
+Employee::Employee() :baseSalary(0) { numOfEmployee++; }
+Employee::Employee(string id, string name, string type, double baseSalary, Attendance attend) :id(id), name(name), type(type), baseSalary(baseSalary), attend(attend) { numOfEmployee++; }
+Employee::~Employee() { numOfEmployee--; }
+
+void Employee::setID(string id) { this->id = id; }
+void Employee::setName(string name) { this->name = name; }
+void Employee::setType(string type) { this->type = type; }
+void Employee::setBaseSalary(double baseSalary) { this->baseSalary = baseSalary; }
+void Employee::setAttendance(int personalLeave, int sickLeave, int lateHour) {
+	attend.setPersonalLeave(personalLeave);
+	attend.setSickLeave(sickLeave);
+	attend.setLateHour(lateHour);
+}
+
+string Employee::getID() const { return id; }
+string Employee::getName() const { return name; }
+string Employee::getType() const { return type; }
+double Employee::getBaseSalary() const { return baseSalary; }
+Attendance Employee::getAttendance() const { return attend; }
+
+double Employee::getBonus() const { return 0; }
+
+void Employee::print() {
+	cout << left << setw(8) << getID()
+		<< setw(16) << getName()
+		<< setw(8) << getType()
+		<< setw(12) << getBaseSalary()
+		<< setw(32) << getAttendance().print()
+		<< setw(8) << getBonus() << endl;
+}
+
+int Employee::getNumOfEmployee() { return numOfEmployee; }
