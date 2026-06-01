97d183c46911c925d57c0c8f0965191eb28c0d78
sweet20potato <candy030496@gmail.com>
2026-06-01 12:45:05 +0800

    Refactor main function for employee management

--- Employee-and-Salary-Management-System/main.cpp
+++ Employee-and-Salary-Management-System/main.cpp
@@ -1,30 +1,97 @@
 #include "Read.h"
+#include "Search.h"
+#include "Change.h"
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
 
-void foundName(vector<Employee>& emps, const string keyword);
-void foundType(vector<Employee>& emps, const string keyword);
-void foundID(vector<Employee>& emps, const string keyword);
-
 int main() {
+
 	Read manage("Employees.csv");
 	manage.loadData();
 	manage.printReport();
 
-	cout << "Search(id/name/type): ";
-	string keyword;
-	while (getline(cin,keyword) && keyword != "end") {
-		if (keyword.empty()) continue;
+	Search search;
+	Change change;
 
-		vector<Employee> &emps = manage.getEmployees();
+	string input;
+	while (true) {
+		cout << "=== Function Description ===" << endl
+			<< "1. Search and edit employee information (find)" << endl
+			<< "2. Add a new employee. (add)" << endl
+			<< "3. Firing (deleting) an employee (del)" << endl
+			<< "4. Show current reports (show)" << endl
+			<< "5. Save and exit (end)" << endl
+			<< "Please enter the target action: ";
 
-		if (keyword.find(" ") != string::npos) {
-			foundName(emps, keyword);
+		getline(cin, input);
+		if (input == "end") break;
+		else if (input == "find") {
+			cout << "lease enter your search keywords (ID / Name / Job Type): ";
+
+			string keyword;
+			getline(cin, keyword);
+			if (keyword.empty()) continue;
+
+			vector<Employee>& originalEmps = manage.getEmployees();
+			vector<Employee> searchEmps;
+
+			if (keyword == "part" || keyword == "full") {
+					searchEmps = search.foundType(originalEmps, keyword);
+			}
+			else if (keyword.find(" ") != string::npos) {
+				searchEmps = search.foundName(originalEmps, keyword);
+			}
+			else if (isdigit(keyword[0])) {
+				searchEmps = search.foundID(originalEmps, keyword);
+			}
+			else {
+				cout << "The input is invalid, please try again." << endl;
+				continue;
+			}
+
+			if (searchEmps.empty()) cout << "Unable to find qualified employees" << endl;
+			else {
+				Employee target;
+				
+				if (searchEmps.size() != 1) {
+					cout << "--- Find the following qualified employees ---" << endl;
+					for (Employee& emp : searchEmps) {
+						emp.print();
+					}
+					cout << "--------------------" << endl;
+
+					cout << "Employee ID being searched: ";
+					string targetID;
+					cin >> targetID;
+					searchEmps = search.foundID(searchEmps, targetID);
+				}
+
+				cout << "The object you want to modify is:" << endl;
+				searchEmps[0].print();
+				cout << "y/n : ";
+				char ans;
+				cin >> ans;
+				if (ans == 'y') {
+					change.modifyEmployee(originalEmps[search.searchIndex(originalEmps,searchEmps[0].getID())]);
+				}
+				else {
+
+				}
+			}
 		}
-		else if (keyword == "part" || keyword == "full") {
-			foundType(emps, keyword);
+		else if (input == "add") {
+
+		}
+		else if (input == "del") {
+
+		}
+		else if (input == "show") {
+			manage.printReport();
+		}
+		else {
+			cout << "Invalid option, please re-enter." << endl;
 		}
 	}
 
