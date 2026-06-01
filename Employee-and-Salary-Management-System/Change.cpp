69a155ccc2bb3407f4f7fb7ee9b63c87ec7cd45f
sweet20potato <candy030496@gmail.com>
2026-06-01 12:42:32 +0800

    Refactor employee modification and add methods
    Refactor employee modification logic and add new methods.

--- Employee-and-Salary-Management-System/Change.cpp
+++ Employee-and-Salary-Management-System/Change.cpp
@@ -1,14 +1,60 @@
 #include "Change.h"
-#include "Employee.h"
 #include <iostream>
+#include <sstream>
 using namespace std;
 
-void Change::promote(Employee& emp) {
-	if (emp.getType() == "part") {
-		emp.setType("full");
-		cout << emp.getName() << " got promoted!" << endl;
+void Change::modifyEmployee(Employee& emp) {
+	if (cin.peek() == '\n') {
+		cin.ignore();
 	}
-	else {
-		cout << emp.getName() << " is already a full-time employee." << endl;
+	//吃掉殘留的換行=>防止初始cout兩次
+
+	while (true) {
+
+		cout << endl << "The employee's original information is as follows: " << endl;
+		emp.print();
+
+		cout << "lease enter the information you want to change (type/name/salary/attend) (Enter [done] to finish editing): ";
+		string keyword;
+		getline(cin, keyword);
+		if (keyword == "done") break;
+		else if (keyword == "type") {
+			if (emp.getType() == "full") emp.setType("part");
+			else emp.setType("full");
+		}
+		else if (keyword == "name") {
+			cout << "Enter the name you want to change it to: ";
+			string newName;
+			getline(cin, newName);
+			emp.setName(newName);
+		}
+		else if (keyword == "salary") {
+			cout << "Enter the salary you want to change it to: ";
+			double newSalary;
+			cin >> newSalary;
+			cin.ignore(); //吃掉殘留的換行=>防止初始cout兩次
+			emp.setBaseSalary(newSalary);
+		}
+		else if (keyword == "attend") {
+			cout << "Enter the attendance you want to change it to: ";
+			string newAttendance;
+			getline(cin, newAttendance);
+			stringstream ss(newAttendance);
+			char flash;
+			int personLeave, sickLeave, lateTimes;
+			ss >> personLeave >> flash >> sickLeave >> flash >> lateTimes;
+
+			emp.setAttendance(personLeave,sickLeave,lateTimes);
+		}
 	}
+	cout << "The employee's information has been changed as follows: ";
+	emp.print();
+}
+
+void Change::addEmployee() {
+
+}
+
+void Change::delEmployee() {
+
 }
