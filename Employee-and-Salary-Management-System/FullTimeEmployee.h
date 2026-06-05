83c0842acc481ec5aff1a4c2c7339f1fc66cd03d
sweet20potato <candy030496@gmail.com>
2026-06-05 14:50:22 +0800

    Add FullTimeEmployee class definition

---
+++ Employee-and-Salary-Management-System/FullTimeEmployee.h
@@ -0,0 +1,14 @@
+#ifndef FULLTIMEEMPLOYEE_H
+#define FULLTIMEEMPLOYEE_H
+
+#include "Employee.h"
+class FullTimeEmployee :public Employee {
+public:
+	FullTimeEmployee(string id,string name, double baseSalary, Attendance attend):Employee(id,name,"full",baseSalary,attend){}
+
+	double getBonus() const override {
+		return 0.0;
+	}
+};
+
+#endif
