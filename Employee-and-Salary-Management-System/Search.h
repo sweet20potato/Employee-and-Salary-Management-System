16717bf84b56573961fbc97c00da046d68cb1f1b
sweet20potato <candy030496@gmail.com>
2026-06-05 14:54:16 +0800

    Update Search class to use unique_ptr for Employee

--- Employee-and-Salary-Management-System/Search.h
+++ Employee-and-Salary-Management-System/Search.h
@@ -2,14 +2,19 @@
 #define SEARCH_H
 
 #include <vector>
+#include <memory>
 #include "Employee.h"
+
 class Search
 {
 public:
-	vector<Employee> foundName(vector<Employee>&, const string);
-	vector<Employee> foundType(vector<Employee>&, const string);
+	vector<Employee> foundName(vector<unique_ptr<Employee>>&, const string);
+	vector<Employee> foundType(vector<unique_ptr<Employee>>&, const string);
+	vector<Employee> foundID(vector<unique_ptr<Employee>>&, const string);
+
 	vector<Employee> foundID(vector<Employee>&, const string);
-	int searchIndex(vector<Employee>&,string);
+
+	int searchIndex(vector<unique_ptr<Employee>>&,string);
 };
 
 #endif
