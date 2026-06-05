2c4c0bb564a3a2f180a34f4ff7fb56734a16fae8
sweet20potato <candy030496@gmail.com>
2026-06-05 17:15:06 +0800

    Refactor Employee class to include bonus attribute

--- Employee-and-Salary-Management-System/Employee.cpp
+++ Employee-and-Salary-Management-System/Employee.cpp
@@ -6,19 +6,30 @@ using namespace std;
 
 int Employee::numOfEmployee = 0;
 
-Employee::Employee() :baseSalary(0) { numOfEmployee++; }
-Employee::Employee(string id, string name, string type, double baseSalary, Attendance attend) :id(id), name(name), type(type), baseSalary(baseSalary), attend(attend) { numOfEmployee++; }
-Employee::~Employee() { numOfEmployee--; }
+Employee::Employee() : baseSalary(0), bonus(0.0) {
+    numOfEmployee++;
+}
+
+Employee::Employee(string id, string name, string type, double baseSalary, Attendance attend)
+    : id(id), name(name), type(type), baseSalary(baseSalary), attend(attend), bonus(0.0) {
+    numOfEmployee++;
+}
+
+Employee::~Employee() {
+    numOfEmployee--;
+}
 
 void Employee::setID(string id) { this->id = id; }
 void Employee::setName(string name) { this->name = name; }
 void Employee::setType(string type) { this->type = type; }
 void Employee::setBaseSalary(double baseSalary) { this->baseSalary = baseSalary; }
 void Employee::setAttendance(int personalLeave, int sickLeave, int lateHour) {
-	attend.setPersonalLeave(personalLeave);
-	attend.setSickLeave(sickLeave);
-	attend.setLateHour(lateHour);
+    attend.setPersonalLeave(personalLeave);
+    attend.setSickLeave(sickLeave);
+    attend.setLateHour(lateHour);
 }
+
+void Employee::setBonus(double bonus) { this->bonus = bonus; }
 
 string Employee::getID() const { return id; }
 string Employee::getName() const { return name; }
@@ -26,20 +37,21 @@ string Employee::getType() const { return type; }
 double Employee::getBaseSalary() const { return baseSalary; }
 Attendance Employee::getAttendance() const { return attend; }
 
-double Employee::getBonus() const { return 0; }
+double Employee::getBonus() const { return bonus; }
+
 double Employee::getFinalSalary() const {
     return attend.getFinalSalary(baseSalary) + getBonus();
 }
 
-
 void Employee::print() {
-	cout << left << setw(8) << getID()
-		<< setw(16) << getName()
-		<< setw(8) << getType()
-		<< setw(12) << getBaseSalary()
-		<< setw(20) << getAttendance().print()
-		<< setw(8) << getBonus()
-		<< setw(12) << getFinalSalary() << endl;	
+    cout << left << setw(8) << getID()
+         << setw(16) << getName()
+         << setw(8) << getType()
+         << setw(12) << getBaseSalary()
+         << setw(20) << getAttendance().print()
+         << setw(8) << getBonus()
+         << setw(12) << getFinalSalary()
+         << endl;
 }
 
 int Employee::getNumOfEmployee() { return numOfEmployee; }
