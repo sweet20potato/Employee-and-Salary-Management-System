b6d027b76c20fc0a7ae07670ff146efde445ec97
sweet20potato <candy030496@gmail.com>
2026-06-05 14:53:54 +0800

    Refactor Search methods to use unique_ptr<Employee>

--- Employee-and-Salary-Management-System/Search.cpp
+++ Employee-and-Salary-Management-System/Search.cpp
@@ -1,27 +1,37 @@
 #include "Search.h"
-vector<Employee> Search::foundName(vector<Employee>& emps, const string keyword) {
+vector<Employee> Search::foundName(vector<unique_ptr<Employee>>& emps, const string keyword) {
 	vector<Employee> newEmployee;
 	
-	for (Employee& employee : emps) {
-		if (keyword == employee.getName()) {
-			newEmployee.push_back(employee);
+	for (auto& employee : emps) {
+		if (employee->getName().find(keyword)!=string::npos) {
+			newEmployee.push_back(*employee);
 		}
 	}
 	return newEmployee;
 }
-vector<Employee> Search::foundType(vector<Employee>& emps, const string keyword) {
+vector<Employee> Search::foundType(vector<unique_ptr<Employee>>& emps, const string keyword) {
 	vector<Employee> newEmployee;
 	
-	for (Employee& employee : emps) {	
-		if (keyword == employee.getType()) {
-			newEmployee.push_back(employee);
+	for (auto& employee : emps) {	
+		if (keyword == employee->getType()) {
+			newEmployee.push_back(*employee);
 		}
 	}
 	return newEmployee;
 }
+vector<Employee> Search::foundID(vector<unique_ptr<Employee>>& emps, const string keyword) {
+	vector<Employee> newEmployee;
+
+	for (auto& employee : emps) {
+		if (keyword == employee->getID()) {
+			newEmployee.push_back(*employee);
+		}
+	}
+	return newEmployee;
+}
+
 vector<Employee> Search::foundID(vector<Employee>& emps, const string keyword) {
 	vector<Employee> newEmployee;
-	
 	for (Employee& employee : emps) {
 		if (keyword == employee.getID()) {
 			newEmployee.push_back(employee);
@@ -30,10 +40,10 @@ vector<Employee> Search::foundID(vector<Employee>& emps, const string keyword) {
 	return newEmployee;
 }
 
-int Search::searchIndex(vector<Employee>& emps, string targetID) {
+int Search::searchIndex(vector<unique_ptr<Employee>>& emps, string targetID) {
 	int index = 0;
-	for (Employee& employee : emps) {
-		if (employee.getID() == targetID) break;
+	for (auto& employee : emps) {
+		if (employee->getID() == targetID) break;
 		index++;
 	}
 	return index;
