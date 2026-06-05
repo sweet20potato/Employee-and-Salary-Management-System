2866699726b10d7b5bab674e215f05b688c54ee2
sweet20potato <candy030496@gmail.com>
2026-06-05 14:44:52 +0800

    Update employee management methods to use unique_ptr

--- Employee-and-Salary-Management-System/Change.h
+++ Employee-and-Salary-Management-System/Change.h
@@ -3,12 +3,16 @@
 
 #include <iostream>
 #include <vector>
+#include <memory>
 #include "Employee.h"
+#include "FullTimeEmployee.h"
+#include "PartTimeEmployee.h"
+#include "Attendance.h"
 class Change {
 public:
 	void modifyEmployee(Employee&);
-	void addEmployee(vector<Employee>&);
-	void delEmployee(vector<Employee>&);
+	void addEmployee(vector<unique_ptr<Employee>>&);
+	void delEmployee(vector<unique_ptr<Employee>>&);
 };
 
 #endif
