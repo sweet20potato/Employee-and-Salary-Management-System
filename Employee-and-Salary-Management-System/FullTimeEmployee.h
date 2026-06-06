93d9183fdf2691083c8732815a5456d22a8cd373
sweet20potato <candy030496@gmail.com>
2026-06-06 19:23:43 +0800

    Modify FullTimeEmployee to set bonus based on performance

--- Employee-and-Salary-Management-System/FullTimeEmployee.h
+++ Employee-and-Salary-Management-System/FullTimeEmployee.h
@@ -4,10 +4,24 @@
 #include "Employee.h"
 class FullTimeEmployee :public Employee {
 public:
-	FullTimeEmployee(string id,string name, double baseSalary, Attendance attend):Employee(id,name,"full",baseSalary,attend){}
+	FullTimeEmployee(string id,string name, double baseSalary, Attendance attend, double bonus):Employee(id,name,"full",baseSalary,attend,bonus){}
 
-	double getBonus() const override {
-		return bonus;
+	void setBonus(double performance) override {
+		double newBonus;
+		// 根據業績表現計算獎金
+		if (performance >= 8) {
+			newBonus = getBaseSalary() * 0.2;
+		}
+		else if (performance >= 6) {
+			newBonus = getBaseSalary() * 0.1;
+		}
+		else if (performance >= 3) {
+			newBonus = getBaseSalary() * 0.05;
+		}
+		else {
+			newBonus = 0.0;
+		}
+		bonus = newBonus;
 	}
 };
 
