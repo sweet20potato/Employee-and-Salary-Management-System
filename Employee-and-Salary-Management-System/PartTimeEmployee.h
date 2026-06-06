ec75e7552cc3a0bac9d43b9b30dbc6aa51578c12
sweet20potato <candy030496@gmail.com>
2026-06-07 00:04:57 +0800

    Implement bonus calculation logic in PartTimeEmployee

--- Employee-and-Salary-Management-System/PartTimeEmployee.h
+++ Employee-and-Salary-Management-System/PartTimeEmployee.h
@@ -7,7 +7,14 @@ public:
 	PartTimeEmployee(string id, string name, double baseSalary, Attendance attend, double bonus) :Employee(id, name, "part", baseSalary, attend, bonus) {}
 
 	void setBonus(double performance) override {
-		
+		int personalLeave = getAttendance().getPersonalLeave();
+		int sickLeave = getAttendance().getSickLeave();
+		if(personalLeave > 0 && sickLeave > 0) {
+			bonus = 0.0; // 兼職員工有請假則無獎金
+		}
+		else {
+			bonus = getBaseSalary() * 0.1; // 沒有請假則獎金為基本薪水的10%
+		}
 	}
 };
 
